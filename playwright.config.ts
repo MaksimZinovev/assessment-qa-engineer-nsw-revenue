410e4ad532922a9d6300000fff4b5ca54f24a724
Maksim Zinovev <maksimzinovevsubmit@gmail.com>
2026-09-21 22:45:11 +1000

    Refactor Playwright configuration and enhance Books API tests: update reporter settings, add new test cases, and remove unused example test

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", "list"], ["line"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -46,8 +46,8 @@ export default defineConfig({
     },
     {
       name: "bdd",
-      testDir, 
-       },  
+      testDir,
+    },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/books.spec.ts
+++ tests/books.spec.ts
@@ -24,23 +24,20 @@ async function getApiContext() {
 
 // Demonstrate navigating and asserting various records and elements from the response body Json array.
 test.describe("Books API Tests", () => {
-
-test("T01 should display a list of available books", async ({}) => {
-  apiContext = await getApiContext();
-  const response = await apiContext.get(BOOKS_ENDPOINT);
-
-
-  expect(response.status()).toBe(200);
-  await expect(response).toBeOK();
-  const books = await response.json();
-  expect(books).toHaveLength(6);
-  if (response) {
-    console.dir(books, { depth: null });
-    console.log(`Response status code: ${response.status()}`);
-  } else {
-    console.error("Response is undefined.");
-  }
-});
+  test("T01 should display a list of available books", async ({}) => {
+    apiContext = await getApiContext();
+    const response = await apiContext.get(BOOKS_ENDPOINT);
+    expect(response.status()).toBe(200);
+    await expect(response).toBeOK();
+    const books = await response.json();
+    expect(books).toHaveLength(6);
+    if (response) {
+      console.dir(books, { depth: null });
+      console.log(`Response status code: ${response.status()}`);
+    } else {
+      console.error("Response is undefined.");
+    }
+  });
 
   test("T02 should take less than 1 second to respond", async () => {
     apiContext = await getApiContext();
@@ -52,10 +49,38 @@ test("T01 should display a list of available books", async ({}) => {
     await expect(response).toBeOK();
     expect(responseTime).toBeLessThan(1000);
 
-      if (response) {
-        console.info(`Response time: ${responseTime} ms `);
-      } else {
-        console.error("Response is undefined.");
-      }
+    if (response) {
+      console.info(`Response time: ${responseTime} ms `);
+    } else {
+      console.error("Response is undefined.");
+    }
   });
-})
+
+  test("T03 should have non-fiction book", async () => {
+    apiContext = await getApiContext();
+
+    const response = await apiContext.get(BOOKS_ENDPOINT);
+    const books = await response.json();
+
+    expect(books).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ type: "non-fiction" }),
+      ]),
+    );
+  });
+  test("T04 every ids should be number", async ({}, testInfo) => {
+    apiContext = await getApiContext();
+
+    const response = await apiContext.get(BOOKS_ENDPOINT);
+    const books = await response.json();
+
+    await testInfo.attach("Books-response.json", {
+      body: Buffer.from(JSON.stringify(books, null, 2)),
+      contentType: "application/json",
+    });
+
+    expect(
+      books.every((book: { id: number }) => typeof book.id === "number"),
+    ).toBe(true);
+  });
+});
